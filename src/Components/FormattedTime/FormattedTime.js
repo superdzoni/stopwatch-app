b48f7af27217bac2450b64ef8ac75b6ad73cf9a8
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-09-30 13:42:14 +0200

    Fix millseconds

--- src/Components/FormattedTime/FormattedTime.js
+++ src/Components/FormattedTime/FormattedTime.js
@@ -6,7 +6,7 @@ const FormattedTime = props => {
       <span>{("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:</span>
       <span>{("0" + Math.floor((props.time / 6000) % 60)).slice(-2)}:</span>
       <span>{("0" + Math.floor((props.time / 100) % 60)).slice(-2)}.</span>
-      <span>{("0" + (props.time / 10) % 100).slice(-2)}</span>
+      <span>{("0" + (props.time / 10) % 100).slice(-3)}</span>
     </div>
 	);
 };
